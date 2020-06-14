31a4e526adf539ed8dfa7a17961598e8eca7a7d9
Sasha Kotlyar <arktronic@gmail.com>
2020-06-14 13:11:01 -0400

    Replace old tracking with new, basic tracking

--- assets/privacy-and-track.js
+++ assets/privacy-and-track.js
@@ -1,46 +1,12 @@
-(function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script")
-;r.type="text/javascript";r.async=true
-;r.src="https://cdn.amplitude.com/libs/amplitude-4.5.2-min.gz.js"
-;r.onload=function(){if(e.amplitude.runQueuedFunctions){
-e.amplitude.runQueuedFunctions()}else{
-console.log("[Amplitude] Error: could not load SDK")}}
-;var i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)
-;function s(e,t){e.prototype[t]=function(){
-this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));return this}}
-var o=function(){this._q=[];return this}
-;var a=["add","append","clearAll","prepend","set","setOnce","unset"]
-;for(var u=0;u<a.length;u++){s(o,a[u])}n.Identify=o;var c=function(){this._q=[]
-;return this}
-;var l=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"]
-;for(var p=0;p<l.length;p++){s(c,l[p])}n.Revenue=c
-;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut",
-"setVersionName","setDomain","setDeviceId","setGlobalUserProperties","identify",
-"clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId","logEventWithTimestamp",
-"logEventWithGroups","setSessionId","resetSessionId"]
-;function v(e){function t(t){e[t]=function(){
-e._q.push([t].concat(Array.prototype.slice.call(arguments,0)))}}
-for(var n=0;n<d.length;n++){t(d[n])}}v(n);n.getInstance=function(e){
-e=(!e||e.length===0?"$default_instance":e).toLowerCase()
-;if(!n._iq.hasOwnProperty(e)){n._iq[e]={_q:[]};v(n._iq[e])}return n._iq[e]}
-;e.amplitude=n})(window,document);
-
-var apiKey = null;
-if (document.location.hostname == 'staging.arktronic.com') {
-    apiKey = 'd259d910f02eda76e1d5a1bee7471fe3';
-}
-if (document.location.hostname == 'arktronic.com' || document.location.hostname == 'www.arktronic.com') {
-    apiKey = '60ec86d4aaa416d9af33502b6d5fb86f';
-}
-
-amplitude.getInstance().init(apiKey, null, { saveEvents: true, includeReferrer: true }, function() {
-    var optOutInfoEl = document.getElementById('is-opted-out-of-amplitude');
-    if (optOutInfoEl) {
-        optOutInfoEl.innerHTML = amplitude.getInstance().options.optOut;
+if (document.location.hostname == 'oldblog.arktronic.com') {
+    window.goatcounter = {
+        endpoint: 'https://count-oldblog.arktronic.com/count',
     }
-});
-
-amplitude.getInstance().logEvent('pageLoad', { 'location': document.location.href });
-
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = '//count-oldblog.arktronic.com/count.js';
+    document.getElementsByTagName('head')[0].appendChild(s);
+}
 
 
 function trackingAnalyticsAgree() {
